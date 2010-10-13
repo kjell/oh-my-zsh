3bc96d7e8f08ce20cf6743e12718e123f3cb62f1
Kjell Olsen <kjell@leanside.com>
2010-10-13 12:04:28 -0500

    Source the homebrew-installed brew completions.

--- plugins/brew/brew.plugin.zsh
+++ plugins/brew/brew.plugin.zsh
@@ -1,4 +1,5 @@
 # add brew completion function to path
-fpath=($ZSH/plugins/brew $fpath)
+# fpath=($ZSH/plugins/brew $fpath)
+source /usr/local/Library/Contributions/brew_zsh_completion.zsh
 autoload -U compinit
 compinit -i
